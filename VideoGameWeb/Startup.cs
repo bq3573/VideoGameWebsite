7f5afc0c68a559d0f34b62e6389bce0929a267e3
Brandon_Quinn <brandon8quinn@gmail.com>
2023-01-10 11:39:40 -0500

    a bunch of stuff

---
+++ VideoGameWeb/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace VideoGameWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add your services here
+            services.AddRazorPages();
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                  name: "signin",
+                  pattern: "signin",
+                  defaults: new { controller = "Account", action = "SignIn" }
+                );
+            });
+        }
+
+
+
+    }
+}
